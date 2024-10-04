33d4dba47f5415b22f61a00bd6ed73f69f10ee46
Richard Hallam <62851758+Richard-Hallam@users.noreply.github.com>
2024-10-04 10:59:07 +0100

    wrote function to Separate characters into a list

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Python script to convert obsidian notes to HTML.
+
+Any .md files are just my own notes used for testing while I write this.
--- mdToHtml.py
+++ mdToHtml.py
@@ -1,8 +1,25 @@
+def file_import(file):
+    with open(file, 'r') as f:
+        return [line.strip() for line in f]
 
 
-def convert_to_html(file):
-    f = open(file, 'r')
-    print(f.read())
+def insert_lines_into_list(files):
+    result = []
+    for file in files:
+        result.extend(file_import(file))
+    return result
 
 
-convert_to_html("Flexbox.md")
+#separates all characters into one list
+def insert_characters_into_list(list):
+    result = []
+    for i in list:
+        for char in i:
+            result.append(char)
+    print(result)
+    return result        
+    
+
+files_to_process = ['flexbox.md']
+#print(insert_lines_into_list(files_to_process))
+insert_characters_into_list(insert_lines_into_list(files_to_process))
